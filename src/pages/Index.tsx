76d0fd2bef45d19bc5253b70000ab844ace87fbd
karlz-t <kkonarski42@gmail.com>
2025-12-02 19:42:30 +0100

    Netlify forms modifications

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,6 +9,21 @@ import { Footer } from "@/components/Footer";
 const Index = () => {
   return (
     <main className="min-h-screen">
+      {/* ===== Hidden Dummy Form for Netlify ===== */}
+      <form
+        name="pitch-deck-access"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        className="hidden"
+      >
+        <input type="hidden" name="form-name" value="pitch-deck-access" />
+        <input name="bot-field" />
+        <input name="name" />
+        <input name="email" />
+        <input name="company" />
+      </form>
+
       <Hero />
       <LearnMore />
       <ExpansionJourney />
